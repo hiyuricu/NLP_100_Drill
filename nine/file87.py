ff6433066e2e2783077b871ec739667670447885
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 15:42:09 +0900

    first commit

--- nine/file87.py
+++ nine/file87.py
@@ -7,5 +7,5 @@ con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for obj in col.find({"url" : "http://twitter.com/monagn_IIDX/status/485830078496927744"}):
-    print obj.decode('utf-8')
+for obj in col.find({"user" : "170626372"}).sort({'freq_rted':-1}).limit(10):
+    print obj
