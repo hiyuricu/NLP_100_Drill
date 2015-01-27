be62ee2a61a6527258464e3d5aee7ab9bfcf5a86
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 15:48:06 +0900

    modified

--- nine/file86.py
+++ nine/file86.py
@@ -7,5 +7,6 @@ con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for obj in col.find({"url" : "http://twitter.com/monagn_IIDX/status/485830078496927744"}):
-    print obj
+for dic in col.find({"url" : "http://twitter.com/monagn_IIDX/status/485830078496927744"}):
+    for k,v in dic.items():
+    	print k,v
