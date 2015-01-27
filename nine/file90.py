8197a3de4229ef36b454fadda9b070a180c78be8
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 16:59:58 +0900

    first commit

--- nine/file90.py
+++ nine/file90.py
@@ -3,6 +3,8 @@
 import sys,json,pymongo
 from pymongo import *
 
+query = unicode(sys.argv[1], "utf-8")
+
 con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
