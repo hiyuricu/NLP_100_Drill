eef641f1d314858f035564d3e90c72d1793918e2
tachibana ryuichi <ryu@fomalhaut.sd.tmu.ac.jp>
2015-01-27 16:12:49 +0900

    バグがある

--- nine/file87_2.py
+++ nine/file87_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 #coding:utf-8
 import sys,json
-from pymongo import Connection
+from pymongo import *
 
 con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for dic in col.find({"user" : "170626372"}).sort({'freq_rted':-1}).limit(10):
+for dic in col.find({"user" : "170626372"}).sort('freq_rted', pymongo.ASCENDING):
     for k,v in dic.items():
         print k,v
