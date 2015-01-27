16f09d051caf11656b1c837e893db7a5f6534014
tachibana ryuichi <ryu@fomalhaut.sd.tmu.ac.jp>
2015-01-27 15:46:57 +0900

    modified

--- nine/file87.py
+++ nine/file87.py
@@ -7,5 +7,6 @@ con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for obj in col.find({"user" : "170626372"}).sort({'freq_rted':-1}).limit(10):
-    print obj
+for dic in col.find({"$query":{"user":"170626372"}, "$orderby":{"freq_rted":1}}).limit(10):
+    for k,v in dic.items():
+        print k,v
