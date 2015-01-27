7bac6a7598627cfb1a5efb73c319f5ec04a2488f
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 16:45:26 +0900

    modified

--- nine/file87_2.py
+++ nine/file87_2.py
@@ -7,6 +7,6 @@ con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for dic in col.find({"user" : "170626372"}).sort('freq_rted', pymongo.ASCENDING):
+for dic in col.find({"user" : "170626372"}).sort('freq_rted', pymongo.ASCENDING).limit(10):
     for k,v in dic.items():
         print k,v
