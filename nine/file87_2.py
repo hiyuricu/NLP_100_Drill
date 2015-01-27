0010b3a610bf859b7e54d84f364ee165b2d258f3
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 16:05:14 +0900

    first commit

---
+++ nine/file87_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#coding:utf-8
+import sys,json
+from pymongo import Connection
+
+con = Connection('localhost', 27017) #コネクション作成
+db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
+col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
+
+for dic in col.find({"user" : "170626372"}).sort({'freq_rted':-1}).limit(10):
+    for k,v in dic.items():
+        print k,v
