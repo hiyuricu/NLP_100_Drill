3fbf34752d72c10be4feba38bf03a4e964fec4bb
ryu <tachibanaryuichi@ryu-no-air.lan>
2015-01-27 13:04:04 +0900

    first commit

--- nine/file81.py
+++ nine/file81.py
@@ -4,7 +4,7 @@ import sys,json
 from pymongo import Connection
 
 #第一引数はtweets100.tsv?
-tweet_data = json.load(open(sys.argv[1])) # jsonの取得。tweet_dataは辞書が要素のリストっぽい
+tweet_data = json.load(open(sys.argv[1])) # jsonの取得。tweet_dataは辞書が要素のリスト
 
 con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
@@ -24,4 +24,5 @@ for one_tw in tweet_data: # one_twは辞書っぽい
     if one_tw["in_reply_to_screen_name"] is not None:
         tw["reply_url"] = "http://twitter.com/"+one_tw["in_reply_to_screen_name"]+"/status/"+one_tw["in_reply_to_status_id_str"]
     tw["freq_rted"] = one_tw["retweet_count"]
+
     col.insert(tw)
