e02af62cfd521e219008f1d491293565e51f0fa4
ryu <tachibanaryuichi@ryu-no-MacBook-Air.local>
2015-02-02 11:25:57 +0900

    modified

--- nine/file90.py
+++ nine/file90.py
@@ -9,6 +9,6 @@ con = Connection('localhost', 27017) #コネクション作成
 db = con.nlp100_tachibana #コネクションからnlp100_tachibanaデータベースを取得
 col = db.tweets #nlp100_tachibanaデータベースからtweetsコレクションを取得
 
-for data in col.find():
+for data in col.find().sort('freq_rted', pymongo.DESCENDING):
     if query in data["body"]:
         print "%s :\n%s" % (data["nickname"], data["body"])
